c4c4ea3d7596bb1b2ca55afe36fa6f9352908ade
KiwookJUNG <ismeeki17@naver.com>
2019-10-16 19:48:19 +0900

    Initial Commit

---
+++ LoginPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginPractice
+//
+//  Created by 정기욱 on 2019/10/16.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
